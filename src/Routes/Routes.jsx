9f1c4d4b02cd2a6cb2164ac2ceed83822879f43d
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-14 23:28:23 +0600

    routs

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Routes = createBrowserRouter([
+    {
+        path:'/',
+        element:<h1>Home Layout</h1>
+    },
+    {
+        path:'/news',
+        element:<
+    }
+])
+export default Routes;
