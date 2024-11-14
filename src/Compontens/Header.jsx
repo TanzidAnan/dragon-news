fc040bd9acc5cbc0fe40c18371dcf3905760f119
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-14 23:50:47 +0600

    heading style

--- src/Compontens/Header.jsx
+++ src/Compontens/Header.jsx
@@ -1,7 +1,7 @@
 import logo from '../assets/logo.png'
 const Header = () => {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col justify-center items-center gap-3'>
             <div className='logo'>
                 <img className='w-[400px]' src={logo} alt="" />
             </div>
