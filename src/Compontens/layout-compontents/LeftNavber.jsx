9c23a1226f1ad964c14981d7632cfac78c1e8030
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-15 02:56:06 +0600

    data load

--- src/Compontens/layout-compontents/LeftNavber.jsx
+++ src/Compontens/layout-compontents/LeftNavber.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const LeftNavber = () => {
     const [categories,setCategories] =useState([]);
-    
+
+    useEffect( () =>{
+        fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res =>res.json())
+        .then(data =>console.log(data.data.news_category))
+    },[])
+
     return (
         <div>
             <h1 className="font-semibold font-poppins text-lg">All Caterogy</h1>
