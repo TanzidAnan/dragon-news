1c54a7e33ffbcb3e2d78a1277b7b18e03ba73c9c
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-15 03:15:25 +0600

    category data display

--- src/Compontens/layout-compontents/LeftNavber.jsx
+++ src/Compontens/layout-compontents/LeftNavber.jsx
@@ -6,12 +6,17 @@ const LeftNavber = () => {
     useEffect( () =>{
         fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res =>res.json())
-        .then(data =>console.log(data.data.news_category))
+        .then(data =>setCategories(data.data.news_category))
     },[])
-
+console.log(categories)
     return (
         <div>
-            <h1 className="font-semibold font-poppins text-lg">All Caterogy</h1>
+            <h1 className="font-semibold font-poppins text-lg">All Caterogy:({categories.length})</h1>
+            <div className="flex flex-col gap-2 mt-9">
+                {
+                    categories.map((categorie,idx) =><button className="btn" key={idx}>{categorie.category_name}</button>)
+                }
+            </div>
         </div>
     );
 };
